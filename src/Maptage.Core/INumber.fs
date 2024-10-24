0b27b9181bb1d8e415eea629aa3abd2170c64750
ricky <ricky.y.c@outlook.com>
2024-10-25 00:30:30 +0800

    update inumber

--- src/Maptage.Core/INumber.fs
+++ src/Maptage.Core/INumber.fs
@@ -1,7 +1,11 @@
 ﻿namespace Maptage.Core
 
 #if NET7_0_OR_GREATER
-type INumber<'num & #System.Numerics.INumber<'num>> = 'num
+type INumber<'num & #System.Numerics.INumber<'num>
+when 'num : comparison
+and 'num : (static member op_Explicit: 'num -> float32)
+and 'num : (static member op_Explicit: 'num -> int)
+> = 'num
 #else
 type INumber<'num
 when 'num : comparison
