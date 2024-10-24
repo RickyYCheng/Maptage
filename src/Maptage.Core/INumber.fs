0f593e357b4848059a2beb26a35cbe77c4a68f16
ricky <ricky.y.c@outlook.com>
2024-10-24 23:12:33 +0800

    add inumber constraint

---
+++ src/Maptage.Core/INumber.fs
@@ -0,0 +1,19 @@
+﻿namespace Maptage.Core
+
+#if NET7_0_OR_GREATER
+type INumber<'num & #System.Numerics.INumber<'num>> = 'num
+#else
+type INumber<'num
+when 'num : comparison
+and 'num : (static member One : 'num)
+and 'num : (static member Zero : 'num)
+and 'num : (static member Abs : 'num -> 'num)
+and 'num : (static member ( ~- ) : 'num -> 'num)
+and 'num : (static member ( + ) : 'num * 'num -> 'num)
+and 'num : (static member ( - ) : 'num * 'num -> 'num)
+and 'num : (static member ( * ) : 'num * 'num -> 'num)
+and 'num : (static member ( / ) : 'num * 'num -> 'num)
+and 'num : (static member op_Explicit: 'num -> float32)
+and 'num : (static member op_Explicit: 'num -> int)
+> = 'num
+#endif
