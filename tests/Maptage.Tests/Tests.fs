46788617b54934f6a0d31933697460d98f19a652
ricky <ricky.y.c@outlook.com>
2024-10-25 20:34:45 +0800

    fix test

--- tests/Maptage.Tests/Tests.fs
+++ tests/Maptage.Tests/Tests.fs
@@ -1,9 +1,16 @@
-module Maptage.Tests
+module Maptage.Tests.Tests
 
 #nowarn "0988"
 
 open Xunit
 open Xunit.Abstractions
 
-type TargetFrameworkHelper(out:ITestOutputHelper) =
-    [<Fact>] member _.``Test target framework``() = Maptage.Core.Literals.TargetFramework |> out.WriteLine
+type TargetFrameworkTester(out:ITestOutputHelper) =
+    #if NET7_0_OR_GREATER
+    let [<Literal>] TargetFramework = "NET 7.0 +"
+    #else
+    let [<Literal>] TargetFramework = "NETSTANDARD 2.1"
+    #endif
+    [<Fact>] member _.``Test target framework``() = TargetFramework |> out.WriteLine
+
+let [<Fact>] ``Default test``() = Assert.True true
