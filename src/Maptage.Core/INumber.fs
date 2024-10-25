cedf02026c531a6e7bf3b339a2f293330b38bd20
ricky <ricky.y.c@outlook.com>
2024-10-25 17:56:42 +0800

    add inline in constraints

--- src/Maptage.Core/INumber.fs
+++ src/Maptage.Core/INumber.fs
@@ -8,14 +8,14 @@ and 'num : (static member inline op_Explicit : float32 -> 'num)
 #else
 type INumber<'num
 when 'num : comparison
-and 'num : (static member One : 'num)
-and 'num : (static member Zero : 'num)
-and 'num : (static member Abs : 'num -> 'num)
-and 'num : (static member ( ~- ) : 'num -> 'num)
-and 'num : (static member ( + ) : 'num * 'num -> 'num)
-and 'num : (static member ( - ) : 'num * 'num -> 'num)
-and 'num : (static member ( * ) : 'num * 'num -> 'num)
-and 'num : (static member ( / ) : 'num * 'num -> 'num)
+and 'num : (static member inline One : 'num)
+and 'num : (static member inline Zero : 'num)
+and 'num : (static member inline Abs : 'num -> 'num)
+and 'num : (static member inline ( ~- ) : 'num -> 'num)
+and 'num : (static member inline ( + ) : 'num * 'num -> 'num)
+and 'num : (static member inline ( - ) : 'num * 'num -> 'num)
+and 'num : (static member inline ( * ) : 'num * 'num -> 'num)
+and 'num : (static member inline ( / ) : 'num * 'num -> 'num)
 and 'num : (static member inline op_Explicit : float32 -> 'num)
 > = 'num
 #endif
