aec965e8094cf515d292d8787081972fc1bd3519
ricky <ricky.y.c@outlook.com>
2024-10-27 19:53:55 +0800

    add method

--- src/Maptage.Core/Geometry/CSpace/CfgLine.fs
+++ src/Maptage.Core/Geometry/CSpace/CfgLine.fs
@@ -7,6 +7,9 @@ open Maptage.Core.Geometry
 [<AbstractClass; Sealed>]
 type CfgLine =
     [<Extension>]
-    static member inline _Translate<'n, 'v when IVector2<'n, 'v>>(cfgLine: 'v CfgLine byref, trans) =
+    static member inline _Translate<'n, 'v when IVector2<'n, 'v>>(cfgLine:'v CfgLine byref, trans) =
         cfgLine.Pos1 <- cfgLine.Pos1.add trans
         cfgLine.Pos2 <- cfgLine.Pos2.add trans
+    [<Extension>]
+    static member inline compare<'n, 'v when IVector2<'n, 'v>>(this:'v CfgLine, ``to``:'v CfgLine) =
+        this.sqLength() - ``to``.sqLength()
