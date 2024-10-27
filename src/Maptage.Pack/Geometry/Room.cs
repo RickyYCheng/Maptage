8ae3fce26a18522b5f403f82477d96b7c788b947
ricky <ricky.y.c@outlook.com>
2024-10-27 16:27:19 +0800

    add ctor for C# record

--- src/Maptage.Pack/Geometry/Room.cs
+++ src/Maptage.Pack/Geometry/Room.cs
@@ -4,10 +4,10 @@ public record struct Room<TVec>(
     TVec[] Vertices,
     TVec CenterShift,
     RoomWall<TVec>[] Walls,
+    bool[] DoorFlags,
     
-    float Energy,
-    int TemplateType,
-    bool FlagFixed,
-    int BoundaryType,
-    bool[] DoorFlags
+    float Energy = 1f,
+    int TemplateType = -1,
+    bool FlagFixed = false,
+    int BoundaryType = 0
 );
