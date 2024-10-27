227bf893a67d39621d5f03f24a3acda1f84da843
ricky <ricky.y.c@outlook.com>
2024-10-27 19:24:44 +0800

    add config line

---
+++ src/Maptage.Core/Geometry/CSpace/CfgLine.fs
@@ -0,0 +1,12 @@
+﻿namespace Maptage.Core.Geometry.CSpace
+
+open System.Runtime.CompilerServices
+open Maptage.Core
+open Maptage.Core.Geometry
+
+[<AbstractClass; Sealed>]
+type CfgLine =
+    [<Extension>]
+    static member inline _Translate<'n, 'v when IVector2<'n, 'v>>(cfgLine: 'v CfgLine byref, trans) =
+        cfgLine.Pos1 <- cfgLine.Pos1.add trans
+        cfgLine.Pos2 <- cfgLine.Pos2.add trans
